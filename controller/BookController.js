1128a76915a80121024cc15b94682a7da10d4276
1eehyerin <danbe8080@gmail.com>
2025-05-21 14:53:06 +0900

    - 도서 신간 목록 조건 추가 - 도서 목록 페이징 처리 추가

--- controller/BookController.js
+++ controller/BookController.js
@@ -2,32 +2,36 @@ const connection = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const allBooks = (req, res) => {
-  let { category_id } = req.query;
+  let { category_id, news, limit, currentPage } = req.query;
+  let offset = limit * (currentPage - 1);
+  let sql = `SELECT * FROM books `;
+  let values = [];
+
   if (category_id) {
-    let sql = "SELECT * FROM books WHERE category_id=?";
-
-    connection.query(sql, category_id, (err, result) => {
-      if (err) {
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      if (result.length) return res.status(StatusCodes.OK).json(result);
-      else return res.status(StatusCodes.NOT_FOUND).end();
-    });
-  } else {
-    let sql = "SELECT * FROM books";
-
-    connection.query(sql, (err, result) => {
-      if (err) {
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      return res.status(StatusCodes.OK).json(result);
-    });
+    sql += "WHERE category_id=? ";
+    values.push(category_id);
   }
+  if (news) {
+    sql += category_id ? "AND" : "WHERE";
+    sql += " pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH ) AND NOW()";
+  }
+
+  sql += ` LIMIT ? OFFSET ?`;
+  values.push(parseInt(limit), offset);
+
+  connection.query(sql, values, (err, result) => {
+    if (err) {
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if (result.length) return res.status(StatusCodes.OK).json(result);
+    else return res.status(StatusCodes.NOT_FOUND).end();
+  });
 };
 
 const bookDetail = (req, res) => {
   const id = parseInt(req.params.id);
-  let sql = "SELECT * FROM books WHERE id=?";
+  let sql =
+    "SELECT * FROM books LEFT JOIN category ON books.category_id=category.id WHERE books.id=?";
 
   connection.query(sql, id, (err, result) => {
     if (err) {
@@ -37,21 +41,5 @@ const bookDetail = (req, res) => {
     else return res.status(StatusCodes.NOT_FOUND).end();
   });
 };
-
-// const categoryBooks = (req, res) => {
-//   let { category_id } = req.query;
-//   if (!category_id) {
-//   }
-
-//   let sql = "SELECT * FROM books WHERE category_id=?";
-
-//   connection.query(sql, category_id, (err, result) => {
-//     if (err) {
-//       return res.status(StatusCodes.BAD_REQUEST).end();
-//     }
-//     if (result.length) return res.status(StatusCodes.OK).json(result);
-//     else return res.status(StatusCodes.NOT_FOUND).end();
-//   });
-// };
 
 module.exports = { allBooks, bookDetail };
